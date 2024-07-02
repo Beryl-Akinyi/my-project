2abc5120497448b37c075e7c3d51446bc2534107
Beryl-Akinyi <berylakodhiambo@gmail.com>
2024-07-02 18:34:56 +0300

    Removed the word query

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function formatDate(date) {
 }
 function searchCity(city) {
   let apiKey = "tdafd5e2a6925f466co34d9ee0fffbdb";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}{query}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 function handleSearchSubmit(event) {
