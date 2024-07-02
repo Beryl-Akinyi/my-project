abc316ac32d87cb8c8d9bc25f9b190d458c3bd20
Beryl-Akinyi <berylakodhiambo@gmail.com>
2024-07-02 18:43:07 +0300

    Made some changes

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function formatDate(date) {
 function searchCity(city) {
   let apiKey = "tdafd5e2a6925f466co34d9ee0fffbdb";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(searchCity);
+  axios.get(apiUrl).then(refreshWeather);
 }
 function handleSearchSubmit(event) {
   event.preventDefault();
